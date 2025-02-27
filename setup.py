d80593d5e2d6b314e901e93ea85dfece8e8b7d40
DMinha <134809350+DMinha@users.noreply.github.com>
2025-02-27 13:50:38 -0500

    SLAM

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'reactive_racing'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jacob Knaup',
+    maintainer_email='jacobk@gatech.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'perception = reactive_racing.perceive_boundaries:main',
+            'planning = reactive_racing.estimate_centerline:main',
+            'control = reactive_racing.compute_control:main',
+            'zed2_custom_node = reactive_racing.zed2_custom_node:main',
+            'joystick_ackermann_teleop = reactive_racing.joystick_ackermann_teleop:main',
+          #'control = reactive_racing.racecar_controller:main',
+        ],
+    },
+)
